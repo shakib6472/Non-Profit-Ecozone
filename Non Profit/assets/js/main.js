cd696e0d7499ded26134478c12a810b1c9ee77c3
shakib6472 <123575818+shakib6472@users.noreply.github.com>
2024-09-14 21:34:40 +0600

    Add files via upload

---
+++ Non Profit/assets/js/main.js
@@ -0,0 +1,588 @@
+(function ($) {
+  'use strict';
+
+  /*
+  |--------------------------------------------------------------------------
+  | Template Name: Ecozone
+  | Author: Laralink
+  | Version: 1.0.0
+  |--------------------------------------------------------------------------
+  |--------------------------------------------------------------------------
+  | TABLE OF CONTENTS:
+  |--------------------------------------------------------------------------
+  |
+  | 1. Preloader
+  | 2. Mobile Menu
+  | 3. Sticky Header
+  | 4. Dynamic Background
+  | 5. Slick Slider
+  | 6. Modal Video
+  | 7. Scroll Up
+  | 8. Donation Card
+  | 9. Ripple
+  | 10. Counter Animation
+  | 11. Accordian
+  | 12. Tabs
+  | 13. Progress Bar
+  | 14. Review
+  | 15. Light Gallery
+  | 16. Round Percent
+  | 17. Ecommerce
+  | 18. Cursor Animation
+  |
+  */
+
+  /*--------------------------------------------------------------
+    Scripts initialization
+  --------------------------------------------------------------*/
+  $.exists = function (selector) {
+    return $(selector).length > 0;
+  };
+
+  $(window).on('load', function () {
+    preloader();
+  });
+
+  $(function () {
+    mainNav();
+    stickyHeader();
+    dynamicBackground();
+    counterInit();
+    slickInit();
+    modalVideo();
+    scrollUp();
+    donationCard();
+    rippleInit();
+    accordian();
+    tabs();
+    progressBar();
+    review();
+    lightGallery();
+    roundPercentInit();
+    ecommerce();
+
+    if ($.exists('.wow')) {
+      new WOW().init();
+    }
+    if ($.exists('.player')) {
+      $('.player').YTPlayer();
+    }
+  });
+
+  $(window).on('scroll', function () {
+    showScrollUp();
+  });
+
+  /*--------------------------------------------------------------
+    1. Preloader
+  --------------------------------------------------------------*/
+  function preloader() {
+    $('.cs_preloader').fadeOut();
+    $('cs_preloader_in').delay(150).fadeOut('slow');
+  }
+
+  /*--------------------------------------------------------------
+    2. Mobile Menu
+  --------------------------------------------------------------*/
+  function mainNav() {
+    $('.cs_nav').append('<span class="cs_menu_toggle"><span></span></span>');
+    $('.menu-item-has-children').append(
+      '<span class="cs_munu_dropdown_toggle"><span></span></span>',
+    );
+    $('.cs_menu_toggle').on('click', function () {
+      $(this)
+        .toggleClass('cs_toggle_active')
+        .siblings('.cs_nav_list')
+        .toggleClass('cs_active');
+    });
+    $('.cs_munu_dropdown_toggle').on('click', function () {
+      $(this).toggleClass('active').siblings('ul').slideToggle();
+      $(this).parent().toggleClass('active');
+    });
+
+    /* Side Nav */
+    $('.cs_hamburger_btn').on('click', function () {
+      $('.cs_side_header').addClass('active');
+      $('html').addClass('cs_hamburger_active');
+    });
+    $('.cs_close, .cs_side_header_overlay').on('click', function () {
+      $('.cs_side_header').removeClass('active');
+      $('html').removeClass('cs_hamburger_active');
+    });
+  }
+
+  /*--------------------------------------------------------------
+    3. Sticky Header
+  --------------------------------------------------------------*/
+  function stickyHeader() {
+    var $window = $(window);
+    var lastScrollTop = 0;
+    var $header = $('.cs_sticky_header');
+    var headerHeight = $header.outerHeight() + 20;
+
+    $window.scroll(function () {
+      var windowTop = $window.scrollTop();
+
+      if (windowTop >= headerHeight) {
+        $header.addClass('cs_gescout_sticky');
+      } else {
+        $header.removeClass('cs_gescout_sticky');
+        $header.removeClass('cs_gescout_show');
+      }
+
+      if ($header.hasClass('cs_gescout_sticky')) {
+        if (windowTop < lastScrollTop) {
+          $header.addClass('cs_gescout_show');
+        } else {
+          $header.removeClass('cs_gescout_show');
+        }
+      }
+      lastScrollTop = windowTop;
+    });
+  }
+
+  /*--------------------------------------------------------------
+    4. Dynamic Background
+  --------------------------------------------------------------*/
+  function dynamicBackground() {
+    $('[data-src]').each(function () {
+      var src = $(this).attr('data-src');
+      $(this).css({
+        'background-image': 'url(' + src + ')',
+      });
+    });
+  }
+
+  /*--------------------------------------------------------------
+    5. Slick Slider
+  --------------------------------------------------------------*/
+  function slickInit() {
+    if ($.exists('.cs_slider')) {
+      $('.cs_slider').each(function () {
+        // Slick Variable
+        var $ts = $(this).find('.cs_slider_container');
+        var $slickActive = $(this).find('.cs_slider_wrapper');
+        // Auto Play
+        var autoPlayVar = parseInt($ts.attr('data-autoplay'), 10);
+        // Auto Play Time Out
+        var autoplaySpdVar = 3000;
+        if (autoPlayVar > 1) {
+          autoplaySpdVar = autoPlayVar;
+          autoPlayVar = 1;
+        }
+        // Slide Change Speed
+        var speedVar = parseInt($ts.attr('data-speed'), 10);
+        // Slider Loop
+        var loopVar = Boolean(parseInt($ts.attr('data-loop'), 10));
+        // Slider Center
+        var centerVar = Boolean(parseInt($ts.attr('data-center'), 10));
+        // Variable Width
+        var variableWidthVar = Boolean(
+          parseInt($ts.attr('data-variable-width'), 10),
+        );
+        // Pagination
+        var paginaiton = $(this)
+          .find('.cs_pagination')
+          .hasClass('cs_pagination');
+        // Slide Per View
+        var slidesPerView = $ts.attr('data-slides-per-view');
+        if (slidesPerView == 1) {
+          slidesPerView = 1;
+        }
+        if (slidesPerView == 'responsive') {
+          var slidesPerView = parseInt($ts.attr('data-add-slides'), 10);
+          var lgPoint = parseInt($ts.attr('data-lg-slides'), 10);
+          var mdPoint = parseInt($ts.attr('data-md-slides'), 10);
+          var smPoint = parseInt($ts.attr('data-sm-slides'), 10);
+          var xsPoing = parseInt($ts.attr('data-xs-slides'), 10);
+        }
+        // Fade Slider
+        var fadeVar = parseInt($($ts).attr('data-fade-slide'));
+        fadeVar === 1 ? (fadeVar = true) : (fadeVar = false);
+
+        // Slick Active Code
+        $slickActive.slick({
+          autoplay: autoPlayVar,
+          dots: paginaiton,
+          centerPadding: '28%',
+          speed: speedVar,
+          infinite: loopVar,
+          autoplaySpeed: autoplaySpdVar,
+          centerMode: centerVar,
+          fade: fadeVar,
+          prevArrow: $(this).find('.cs_left_arrow'),
+          nextArrow: $(this).find('.cs_right_arrow'),
+          appendDots: $(this).find('.cs_pagination'),
+          slidesToShow: slidesPerView,
+          variableWidth: variableWidthVar,
+          swipeToSlide: true,
+          responsive: [
+            {
+              breakpoint: 1600,
+              settings: {
+                slidesToShow: lgPoint,
+              },
+            },
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: mdPoint,
+              },
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: smPoint,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: xsPoing,
+              },
+            },
+          ],
+        });
+      });
+    }
+  }
+
+  /*--------------------------------------------------------------
+    6. Modal Video
+  --------------------------------------------------------------*/
+  function modalVideo() {
+    if ($.exists('.cs_video_open')) {
+      $('body').append(`
+        <div class="cs_video_popup">
+          <div class="cs_video_popup-overlay"></div>
+          <div class="cs_video_popup-content">
+            <div class="cs_video_popup-layer"></div>
+            <div class="cs_video_popup-container">
+              <div class="cs_video_popup-align">
+                <div class="embed-responsive embed-responsive-16by9">
+                  <iframe class="embed-responsive-item" src="about:blank"></iframe>
+                </div>
+              </div>
+              <div class="cs_video_popup-close"></div>
+            </div>
+          </div>
+        </div>
+      `);
+      $(document).on('click', '.cs_video_open', function (e) {
+        e.preventDefault();
+        var video = $(this).attr('href');
+
+        $('.cs_video_popup-container iframe').attr('src', `${video}`);
+
+        $('.cs_video_popup').addClass('active');
+      });
+      $('.cs_video_popup-close, .cs_video_popup-layer').on(
+        'click',
+        function (e) {
+          $('.cs_video_popup').removeClass('active');
+          $('html').removeClass('overflow-hidden');
+          $('.cs_video_popup-container iframe').attr('src', 'about:blank');
+          e.preventDefault();
+        },
+      );
+    }
+  }
+
+  /*--------------------------------------------------------------
+    7. Scroll Up
+  --------------------------------------------------------------*/
+  function scrollUp() {
+    $('.cs_scrollup').on('click', function (e) {
+      e.preventDefault();
+      $('html,body').animate(
+        {
+          scrollTop: 0,
+        },
+        0,
+      );
+    });
+  }
+  /* For Scroll Up */
+  function showScrollUp() {
+    let scroll = $(window).scrollTop();
+    if (scroll >= 350) {
+      $('.cs_scrollup').addClass('cs_scrollup_show');
+    } else {
+      $('.cs_scrollup').removeClass('cs_scrollup_show');
+    }
+  }
+
+  /*--------------------------------------------------------------
+    8. Donation Card
+  --------------------------------------------------------------*/
+  function donationCard() {
+    // Update the value when a different radio button is selected
+    $('.cs_fixed_amount_check').on('change', function () {
+      var selectedValue = $(this).val();
+      $('.cs_input_amount').val('$' + selectedValue);
+    });
+
+    // Handle the custom amount button click
+    $('.cs_custom_amount_btn[type="button"]').on('click', function () {
+      $('.cs_input_amount').prop('disabled', false).val('').focus();
+      $('.cs_fixed_amount_check').prop('checked', false);
+    });
+  }
+  /*--------------------------------------------------------------
+    9. Ripple
+  --------------------------------------------------------------*/
+  function rippleInit() {
+    if ($.exists('.cs_ripple_activate')) {
+      $('.cs_ripple_activate').each(function () {
+        $('.cs_ripple_activate').ripples({
+          resolution: 512,
+          dropRadius: 20,
+          perturbance: 0.04,
+        });
+      });
+    }
+  }
+
+  /*--------------------------------------------------------------
+    10. Counter Animation
+  --------------------------------------------------------------*/
+  function counterInit() {
+    if ($.exists('.odometer')) {
+      $(window).on('scroll', function () {
+        function winScrollPosition() {
+          var scrollPos = $(window).scrollTop(),
+            winHeight = $(window).height();
+          var scrollPosition = Math.round(scrollPos + winHeight / 1.2);
+          return scrollPosition;
+        }
+
+        $('.odometer').each(function () {
+          var elemOffset = $(this).offset().top;
+          if (elemOffset < winScrollPosition()) {
+            $(this).html($(this).data('count-to'));
+          }
+        });
+      });
+    }
+  }
+
+  /*--------------------------------------------------------------
+    11. Accordian
+  --------------------------------------------------------------*/
+  function accordian() {
+    $('.cs_accordian').children('.cs_accordian_body').hide();
+    $('.cs_accordian.active').children('.cs_accordian_body').show();
+    $('.cs_accordian_head').on('click', function () {
+      $(this)
+        .parent('.cs_accordian')
+        .siblings()
+        .children('.cs_accordian_body')
+        .slideUp(250);
+      $(this).siblings().slideDown(250);
+      $(this)
+        .parent()
+        .parent()
+        .siblings()
+        .find('.cs_accordian_body')
+        .slideUp(250);
+      /* Accordian Active Class */
+      $(this).parents('.cs_accordian').addClass('active');
+      $(this).parent('.cs_accordian').siblings().removeClass('active');
+    });
+  }
+
+  /*--------------------------------------------------------------
+    12. Tabs
+  --------------------------------------------------------------*/
+  function tabs() {
+    $('.cs_tabs .cs_tab_links a').on('click', function (e) {
+      var currentAttrValue = $(this).attr('href');
+      $('.cs_tabs ' + currentAttrValue)
+        .fadeIn(400)
+        .siblings()
+        .hide();
+      $(this).parents('li').addClass('active').siblings().removeClass('active');
+      e.preventDefault();
+    });
+  }
+
+  /*--------------------------------------------------------------
+    13. Progress Bar
+  --------------------------------------------------------------*/
+  function progressBar() {
+    $('.cs_progress').each(function () {
+      var progressPercentage = $(this).data('progress') + '%';
+      $(this).find('.cs_progress_in').css('width', progressPercentage);
+    });
+  }
+
+  /*--------------------------------------------------------------
+    14. Review
+  --------------------------------------------------------------*/
+  function review() {
+    $('.cs_rating').each(function () {
+      var review = $(this).data('rating');
+      var reviewVal = review * 20 + '%';
+      $(this).find('.cs_rating_percentage').css('width', reviewVal);
+    });
+  }
+
+  /*--------------------------------------------------------------
+    15. Light Gallery
+  --------------------------------------------------------------*/
+  function lightGallery() {
+    $('.cs_gallery_list').each(function () {
+      $(this).lightGallery({
+        selector: '.cs_gallery_item',
+        subHtmlSelectorRelative: false,
+        thumbnail: false,
+        mousewheel: true,
+      });
+    });
+  }
+
+  /*--------------------------------------------------------------
+    16. Round Percent
+  --------------------------------------------------------------*/
+  function roundPercentInit() {
+    if ($.exists('.cs_round_progress_percentage')) {
+      $(window).on('scroll', function () {
+        function winScrollPosition() {
+          var scrollPos = $(window).scrollTop(),
+            winHeight = $(window).height();
+          var scrollPosition = Math.round(scrollPos + winHeight / 1.2);
+          return scrollPosition;
+        }
+
+        $('.cs_round_progress_percentage').each(function () {
+          var roundEffect = $(this).offset().top;
+          if (roundEffect < winScrollPosition()) {
+            $(this).each(function () {
+              let roundRadius = $(this).find('circle').attr('r');
+              let roundPercent = $(this).data('percent');
+              let roundCircum = 2 * roundRadius * Math.PI;
+              let roundDraw = (roundPercent * roundCircum) / 100 - 3;
+              $(this).css('stroke-dasharray', roundDraw + ' 999');
+            });
+          }
+        });
+      });
+    }
+  }
+
+  /*--------------------------------------------------------------
+    17. Ecommerce
+  --------------------------------------------------------------*/
+  function ecommerce() {
+    // Star Rating Input
+    $('.cs_input_rating i').on('click', function () {
+      $(this).siblings().removeClass('fa-solid');
+      $(this).addClass('fa-solid').prevAll().addClass('fa-solid');
+    });
+    // Product Single Slider
+    if ($.exists('.cs_single_product_thumb')) {
+      $('.cs_single_product_thumb').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.cs_single_product_nav',
+      });
+    }
+
+    if ($.exists('.cs_single_product_nav')) {
+      $('.cs_single_product_nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.cs_single_product_thumb',
+        focusOnSelect: true,
+        arrows: false,
+      });
+    }
+    // Check All
+    $('#checkAll').change(function () {
+      var isChecked = $(this).prop('checked');
+      $('table input[type="checkbox"]').prop('checked', isChecked);
+    });
+    // Range Slider
+    if ($.exists('#slider-range')) {
+      $('#slider-range').slider({
+        range: true,
+        min: 0,
+        max: 1000,
+        values: [100, 600],
+        slide: function (event, ui) {
+          $('#amount').val('Price: $' + ui.values[0] + ' - $' + ui.values[1]);
+        },
+      });
+    }
+    if ($.exists('#amount')) {
+      $('#amount').val(
+        'Price: $' +
+          $('#slider-range').slider('values', 0) +
+          ' - $' +
+          $('#slider-range').slider('values', 1),
+      );
+    }
+    // Counter
+    $('.cs_increment').click(function () {
+      var countElement = $(this).siblings('.cs_quantity_input');
+      var count = parseInt(countElement.text());
+      count++;
+      countElement.text(count);
+    });
+
+    $('.cs_decrement').click(function () {
+      var countElement = $(this).siblings('.cs_quantity_input');
+      var count = parseInt(countElement.text());
+      if (count > 0) {
+        count--;
+        countElement.text(count);
+      }
+    });
+  }
+
+  /*--------------------------------------------------------------
+    18. Cursor Animation
+  --------------------------------------------------------------*/
+  // Start Cursor Pointer
+  $(function () {
+    $('body').append('<span class="cs_cursor_lg d"></span>');
+    $('body').append('<span class="cs_cursor_sm"></span>');
+
+    $('.cs_view_mouse').on('mouseenter', function () {
+      $('.cs_cursor_lg').addClass('opacity-0');
+      $('.cs_cursor_sm').addClass('opacity-0');
+    });
+    $('.cs_view_mouse').on('mouseleave', function () {
+      $('.cs_cursor_lg').removeClass('opacity-0');
+      $('.cs_cursor_sm').removeClass('opacity-0');
+    });
+  });
+
+  function cursorMovingAnimation(event) {
+    try {
+      const timing = gsap.timeline({
+        defaults: {
+          x: event.clientX,
+          y: event.clientY,
+        },
+      });
+
+      timing
+        .to('.cs_cursor_lg', {
+          ease: 'power2.out',
+        })
+        .to(
+          '.cs_cursor_sm',
+          {
+            ease: 'power2.out',
+          },
+          '-=0.4',
+        );
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  document.addEventListener('mousemove', cursorMovingAnimation);
+  // End Cursor Pointer
+})(jQuery); // End of use strict
